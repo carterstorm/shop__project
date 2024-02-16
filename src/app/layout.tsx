42b8d5519cd45d1aeb10a981546ed8894dc3fd19
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-16 23:55:58 +0100

    Navigation (#3)
    * modified: config files
    
    added prettier, eslint, tailwindcss, and typescript config files
    
    * added: paragraph
    
    created paragraph to check it is working at server
    
    * added: static products
    
    added new files for components, added styles, types and util formatMoney function
    
    * added: Navigation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "@/ui/organisms/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Navigation />
+				<section className="mx-auto max-w-2xl px-8 py-12 sm:px-6 sm:py-16 md:max-w-4xl lg:max-w-7xl">
+					{children}
+				</section>
+			</body>
 		</html>
 	);
 }
