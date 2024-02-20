b7302ca2634576a482cf8171f576340e3ee19db4
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-20 16:28:45 +0100

    server test

--- src/ui/atoms/Logo.tsx
+++ src/ui/atoms/Logo.tsx
@@ -1,3 +1,1 @@
-import NextImage from "next/image";
-
-export const Logo = () => <NextImage src="/logo.svg" alt="Logo" width={50} height={50} />;
+export const Logo = () => <img src="/logo.svg" alt="Logo" width={50} height={50} />;
