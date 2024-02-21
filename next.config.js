f783319f6260b064b45fb0c0bccf2a7e1b3136bf
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-21 17:09:19 +0100

    created privacy and terms static pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	pageExtensions: ["ts", "tsx", "mdx"],
 	experimental: {
 		typedRoutes: true,
+		mdxRs: true,
 	},
 	images: {
 		remotePatterns: [
@@ -27,4 +29,5 @@ const nextConfig = {
 	},
 };
 
-export default nextConfig;
+const withMDX = require("@next/mdx")();
+module.exports = withMDX(nextConfig);
