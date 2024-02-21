1b6eaac8139a79ef73ce00275e0c5509007074c7
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-21 16:03:23 +0100

    added seo for single product

--- src/ui/atoms/SinglePageProductImage.tsx
+++ src/ui/atoms/SinglePageProductImage.tsx
@@ -1,14 +1,9 @@
-import NextImage from "next/image";
 import { type ProductItemResponse } from "@/types";
 
 export const SinglePageProductImage = ({ product }: { product: ProductItemResponse }) => {
 	return (
 		<div className="flex justify-center">
-			<NextImage
-				src={product.image}
-				alt={product.title}
-				className="w-96 rounded-lg object-contain"
-			/>
+			<img src={product.image} alt={product.title} className="w-96 rounded-lg object-contain" />
 		</div>
 	);
 };
